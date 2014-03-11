63cb612f459b63cdf04936b00b3c1f4ca30107cc
Jason Noble <github+jasonn@jasonnoble.org>
2014-03-10 20:41:25 -0600

    End of class Saturday

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,18 +1,45 @@
 class CarsController < ApplicationController
-  def index
+  before_action :find_car, only: [:edit, :update, :destroy]
 
+  def index
+    @cars = Car.all
   end
 
   def new
     @car = Car.new
   end
 
+  def edit
+  end
+
+  def update
+    if @car.update(car_params)
+      redirect_to cars_path, notice: "#{@car.year} #{@car.make} #{@car.model} updated"
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @car.destroy
+    redirect_to cars_path, notice: "#{@car.year} #{@car.make} #{@car.model} was removed"
+  end
+
   def create
     @car = Car.new(car_params)
-    redirect_to cars_path, notice: "#{@car.year} #{@car.make} #{@car.model} created"
+    if @car.save
+      redirect_to cars_path, notice: "#{@car.year} #{@car.make} #{@car.model} created"
+    else
+      render :new
+    end
   end
 
   private
+  def find_car
+    @car = Car.find(params[:id])
+  end
+
   def car_params
     params.require(:car).permit(:make, :model, :year, :price)
   end
--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe Car do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    describe "price" do
+      it "validates presence" do
+        car = Car.new(make: 'Ford', model: 'Fusion', year: 2007, price: 2350.00)
+
+        # Runs car.valid? and verifies it returns true
+        expect(car).to be_valid
+
+        car.price = nil
+        expect(car).to_not be_valid
+        expect(car.errors[:price]).to be_present
+      end
+
+      it "validates value is between 0.01 and 999999.99" do
+        car = Car.new(make: 'Ford', model: 'Fusion', year: 2007, price: 2350.00)
+
+        expect(car).to be_valid
+
+        car.price = 0.00
+        expect(car).to_not be_valid
+        expect(car.errors[:price]).to be_present
+
+        car.price = 1_000_000.00
+        expect(car).to_not be_valid
+        expect(car.errors[:price]).to be_present
+      end
+    end
+  end
 end
